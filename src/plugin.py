9e0b790f2502b712a68054d80c77fb0c40bda91a
Tommy McNeely <tmcneely@deliveryagent.com>
2014-11-06 20:29:21 -0800

    Issue #13 - cleaning up debug logging
    This just got rid of completely redundant logging lines and put more
    info (detail) on the only line it makes sense.

--- src/plugin.py
+++ src/plugin.py
@@ -319,18 +319,16 @@ class NFSPlugin(object):
             response.close()
 
         except httplib.HTTPException, e:
-            self.logger.error(e)
-            self.logger.debug("HTTP Exception: %s" % e)
+            self.logger.error('HTTP Exception: %s' % e)
             pass    # continue on this error
 
         except urllib2.HTTPError, e:
-            self.logger.error(e)
-            self.logger.debug("HTTP Error: %s" % e.read())
+            self.logger.error('HTTP Error: %s' % e)
+            self.logger.debug('HTTP Error Details: %s' % e.read())
             pass    # continue
 
         except urllib2.URLError, e:
             # URLError (DNS Error?)
-            self.logger.error(e)
-            self.logger.debug('URL Error: %s' % e.reason)
+            self.logger.error('URL Error: %s' % e.reason)
             pass    # continue
         self._reset_json_data()
